5667e0abd15a2d1ccd7d60c298842bd1c13541a5
Faisal Ahmad <faisal.idreesi@gmail.com>
2017-01-27 06:59:41 +0500

    Moved index.html to src folder to avoid jekyll picking it up

--- config/paths.js
+++ config/paths.js
@@ -8,7 +8,7 @@ module.exports = {
   root: resolve('../'),
   appSrc: resolve('../src'),
   appBuild: resolve('../app'),
-  appHtml: resolve('../index.html'),
+  appHtml: resolve('../src/index.html'),
   appFavicon: resolve('../favicon.ico'),
   appPackageJson: resolve('../package.json'),
   nodeModules: resolve('../node_modules')
